afc9e1b1a985dfb519f4cce0a5af09673625e795
felipe4253 <felipe4253@gmail.com>
2013-04-25 13:42:17 -0300

    Conclusão dos DAOs

--- src/br/com/vanmais/bancodedados/dao/TipoVeiculoDAO.java
+++ src/br/com/vanmais/bancodedados/dao/TipoVeiculoDAO.java
@@ -6,7 +6,6 @@ package br.com.vanmais.bancodedados.dao;
 import org.hibernate.Session;
 
 import br.com.vanmais.bancodedados.entidade.TipoVeiculoEntidade;
-import br.com.vanmais.bancodedados.interfaces.TipoVeiculoDAOInterface;
 import br.com.vanmais.bancodedados.util.dao.DAOGenerico;
 
 /**
@@ -14,7 +13,7 @@ import br.com.vanmais.bancodedados.util.dao.DAOGenerico;
  *
  */
 public class TipoVeiculoDAO extends DAOGenerico<TipoVeiculoEntidade, Long>
-implements TipoVeiculoDAOInterface{
+{
 
 	public TipoVeiculoDAO(Session sessao) {
 		super(sessao);
--- src/br/com/vanmais/bancodedados/dao/VeiculoDAO.java
+++ src/br/com/vanmais/bancodedados/dao/VeiculoDAO.java
@@ -6,7 +6,6 @@ package br.com.vanmais.bancodedados.dao;
 import org.hibernate.Session;
 
 import br.com.vanmais.bancodedados.entidade.VeiculoEntidade;
-import br.com.vanmais.bancodedados.interfaces.VeiculoDAOInterface;
 import br.com.vanmais.bancodedados.util.dao.DAOGenerico;
 
 /**
@@ -14,7 +13,7 @@ import br.com.vanmais.bancodedados.util.dao.DAOGenerico;
  *
  */
 public class VeiculoDAO extends DAOGenerico<VeiculoEntidade, Long>
-implements VeiculoDAOInterface{
+{
 
 	public VeiculoDAO(Session sessao) {
 		super(sessao);
