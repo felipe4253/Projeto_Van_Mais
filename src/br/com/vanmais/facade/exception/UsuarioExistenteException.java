fb096e55cf4784e3505d19ff4c94210768414f3d
felipe4253 <felipe4253@gmail.com>
2013-05-17 12:28:10 -0300

    alteracoes???

---
+++ src/br/com/vanmais/facade/exception/UsuarioExistenteException.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package br.com.vanmais.facade.exception;
+
+/**
+ * @author <a href = "mailto:felipe4253@gmail.com">Felipe Silva</a>
+ *
+ */
+public class UsuarioExistenteException extends Exception {
+	
+}
