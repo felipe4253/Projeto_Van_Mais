268b138c6e71238c19c7d2072485dd9634e6965c
Felipe4253 <felipe4253@gmail.com>
2013-05-20 04:49:30 -0300

    Alteração motoristabo

--- src/br/com/vanmais/facade/MotoristaBo.java
+++ src/br/com/vanmais/facade/MotoristaBo.java
@@ -10,23 +10,44 @@ import org.hibernate.Session;
 import br.com.vanmais.bancodedados.dao.MotoristaDAO;
 import br.com.vanmais.bancodedados.entidade.MotoristaEntidade;
 import br.com.vanmais.bancodedados.util.HibernateUtil;
+import br.com.vanmais.facade.exception.MotoristaExistenteException;
 
 /**
  * @author <a href = "mailto:felipe4253@gmail.com">Felipe Silva</a>
- *
+ * 
  */
 public class MotoristaBo {
-	
+
 	private MotoristaDAO dao;
 	private Session sessao;
-	
-	public List<MotoristaEntidade> buscarTodos(){
+
+	public List<MotoristaEntidade> buscarTodos() {
 		sessao = HibernateUtil.getSession(sessao);
 		dao = new MotoristaDAO(sessao);
 		List<MotoristaEntidade> lista = dao.buscarTodos();
 		return lista;
 	}
-	
-	public MotoristaEntidade 
-	
+
+	public Long criar(MotoristaEntidade motorista)
+			throws MotoristaExistenteException {
+
+		sessao = HibernateUtil.getSession(sessao);
+		dao = new MotoristaDAO(sessao);
+		List<MotoristaEntidade> motoristas = dao.buscarTodos();
+
+		// Verifica se existe algum motorista com os dados passados
+		for (MotoristaEntidade entidade : motoristas) {
+			if (motorista.getCpf() == entidade.getCpf()) {
+				throw new MotoristaExistenteException();
+			}
+
+			if (motorista.getEmailMotorista().equals(
+					entidade.getEmailMotorista())) {
+				throw new MotoristaExistenteException();
+			}
+		}
+
+		return dao.salvar(motorista);
+	}
+
 }
